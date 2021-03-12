c2738d89962a9f62735a4da7e5a74e214a9d700b
Ravi Patel <ravikano1997@gmail.com>
2021-03-12 15:49:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CPU_Scheduling_Algorithms
+ 
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Scheduling Algorithm',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scheduling Algorithm'),
+      ),
+      //body: Text('Hello World!!'),
+    );
+  }
+}
